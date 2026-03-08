c5a4fa34fd3ae8136915f27cae2137cce3ea1b7d
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-03-08 08:29:36 +0530

    vercel deployment fix

--- service/server.js
+++ service/server.js
@@ -14,5 +14,4 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/students", require("./routes/studentRoutes"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
